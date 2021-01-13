b31d7b90eca59f94e9035066a7453888ce55f959
juan <aragonlopezjuan87@gmail.com>
2021-01-13 19:24:34 +0100

    component add todo

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -1,7 +1,36 @@
-import React, {createContext, useState} from 'react'
+import React, { useState} from 'react'
 
-const AppContext = createContext({})
-
-const AppContextProvider = (props : {children: React.ReactElement})=>{
-    const 
+type Props = {
+    saveTodo: (e: React.FormEvent, formDate: ITodo| any)=> void
 }
+
+const AddTodo: React.FC<Props> = ({saveTodo})=>{
+    const [formData, setFormData] = useState<ITodo | {}>()
+
+//--------------------- DUDA---------------------------//
+
+    const handleForm = (e: React.FormEvent<HTMLInputElement>): void =>{
+        setFormData({
+            ...formData, 
+            [e.currentTarget.id]: e.currentTarget.value,
+        })
+    }
+
+    return (
+        <form onSubmit={(e)=>saveTodo(e, formData)}>
+            <div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input onChange={handleForm} type="text" id='name'/>
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <input onChange={handleForm} type="text" id='description'/>
+                </div>
+                <button disabled={formData === undefined ? true : false}>AddTodo</button>
+            </div>
+        </form>
+    )
+}
+
+export default AddTodo 
